7944b51fa38a99fb7fe9d88c8d4adb8abc385f60
gabrielSabato <88863473+gabrielSabato@users.noreply.github.com>
2021-08-27 15:22:11 -0300

    Update functions.c

--- exe03/functions.c
+++ exe03/functions.c
@@ -1,7 +1,20 @@
 
 #include "functions.h"
 
-int somar(int n1, int n2){
-
-    return n1+n2;
+void menor_maior(int v[],int n, int *menor, int *maior){
+    menor = v[1];
+    maior = v[1];
+    //USA FOR E IF PARA VERIFICAR MAIOR E MENOR ELEMENTO
+    for(int i=0;i<n;i++){
+        if(v[i]<menor){
+            menor = v[i];
+        }
+        else if(v[i]>maior){
+            maior = v[i];
+        }
+    }
+    //IMPRIME OS VALORES
+    printf("MENOR = %d\n",menor);
+    printf("MAIOR = %d\n",maior);
 }
+
