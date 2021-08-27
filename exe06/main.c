3365320b9afebc4d699d6196904206d1b14527d9
gabrielSabato <88863473+gabrielSabato@users.noreply.github.com>
2021-08-27 15:48:59 -0300

    Update main.c

--- exe06/main.c
+++ exe06/main.c
@@ -1,18 +1,22 @@
 #include <stdio.h>
 #include "functions.h"
 
-int main() {
-
-    int a, b, s;
-  
-  // ENTRADA DE DADOS
-  scanf("%d %d", &a, &b);
-
-  // PROCESSAMENTO - EXECUTANDO A FUNCAO
-  s = somar(a, b);
-
-  // SAIDA - IMPRIMINDO O RESULTADO
-  printf("SOMA = %d\n", s);
-
-  return(0);
+int main()
+{
+    //DECLARA AS VARIAVEIS.
+    int b,e,r;
+    //APRESENTA A FUNCAO DO ALGORITMO E SOLICITA A ENTRADA DE DADOS.
+    printf("Potencia de numeros!\n");
+    printf("Digite a base.\n");
+    //ENTRADA DE DADOS.
+    scanf("%d",&b);
+    //SOLICITA ENTRADA DE DADOS
+    printf("Digite o expoente.\n");
+    //ENTRADA DE DADOS
+    scanf("%d",&e);
+    //EXECUTA A FUNCAO
+    r = potencia(b,e);
+    //IMPRIME RESULTADO
+    printf("%d",r);
+    return 0;
 }
