32a9d92d23ba208adffffa4cee507dbc758ab9d7
gabrielSabato <88863473+gabrielSabato@users.noreply.github.com>
2021-08-27 14:19:31 -0300

    Update main.c

--- exe01/main.c
+++ exe01/main.c
@@ -1,18 +1,18 @@
 #include <stdio.h>
 #include "functions.h"
 
-int main() {
-
-    int a, b, s ;
-  
-  // ENTRADA DE DADOS
-  scanf("%d %d", &a, &b);
-
-  // PROCESSAMENTO - EXECUTANDO A FUNCAO
-  s = somar(a, b);
-
-  // SAIDA - IMPRIMINDO O RESULTADO
-  printf("SOMA = %d\n", s);
-
-  return(0);
+int main()
+{
+    //DECLARA AS VARIAVEIS.
+    int n1,n2,s;
+    //APRESENTA A FUNCAO DO ALGORITMO E SOLICITA A ENTRADA DE DADOS.
+    printf("Soma de dois numeros!\n");
+    printf("Digite dois numeros.\n");
+    //ENTRADA DE DADOS.
+    scanf("%d %d",&n1, &n2);
+    //EXECUTA A FUNCAO
+    s = soma(n1,n2);
+    //IMPRIME A RESPOSTA
+    printf("SOMA: %d",s);
+    return 0;
 }
