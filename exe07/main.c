bfc20ce0bac07df9022e4eeb6b116342f6c871de
gabrielSabato <88863473+gabrielSabato@users.noreply.github.com>
2021-08-27 16:14:20 -0300

    Update main.c

--- exe07/main.c
+++ exe07/main.c
@@ -1,18 +1,18 @@
 #include <stdio.h>
 #include "functions.h"
 
-int main() {
-
-    int a, b, s;
-  
-  // ENTRADA DE DADOS
-  scanf("%d %d", &a, &b);
-
-  // PROCESSAMENTO - EXECUTANDO A FUNCAO
-  s = somar(a, b);
-
-  // SAIDA - IMPRIMINDO O RESULTADO
-  printf("SOMA = %d\n", s);
-
-  return(0);
+int main()
+{
+    //DECLARA AS VARIAVEIS.
+    int b,e,r;
+    //APRESENTA A FUNCAO DO ALGORITMO E SOLICITA A ENTRADA DE DADOS.
+    printf("Calculo de MDC!\n");
+    printf("Digite os numeros.\n");
+    //ENTRADA DE DADOS.
+    scanf("%d %d",&b,&e);
+    //EXECUTA A FUNCAO
+    r = mdc(b,e);
+    //IMPRIME RESULTADO
+    printf("%d",r);
+    return 0;
 }
