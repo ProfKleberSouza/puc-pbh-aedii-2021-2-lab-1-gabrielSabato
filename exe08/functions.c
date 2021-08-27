23483b12ff3d1787a22cbd6cf1d463bd5411decc
gabrielSabato <88863473+gabrielSabato@users.noreply.github.com>
2021-08-27 16:36:07 -0300

    Update functions.c

--- exe08/functions.c
+++ exe08/functions.c
@@ -1,7 +1,25 @@
 
 #include "functions.h"
+#include <string.h>
 
-int somar(int n1, int n2){
-
-    return n1+n2;
+void palindromo(char texto[]){
+    int tam;
+    double pal;
+    tam = strlen(texto)-1;
+    for(int i=tam;i>0;i--){
+        for(int j=0;j<tam;j++){
+            if(texto[i]!=texto[j]){
+                pal = 0;
+            }
+            else{
+                pal = 1;
+            }
+        }
+    }
+    if(pal == 1){
+        printf("SIM");
+    }
+    else{
+        printf("NAO");
+    }
 }
