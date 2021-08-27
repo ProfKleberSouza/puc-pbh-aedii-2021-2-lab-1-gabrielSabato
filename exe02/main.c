819b804cbf191d17ef926e1b5072dcb755455f59
gabrielSabato <88863473+gabrielSabato@users.noreply.github.com>
2021-08-27 15:08:36 -0300

    Update main.c

--- exe02/main.c
+++ exe02/main.c
@@ -1,18 +1,25 @@
 #include <stdio.h>
+#include <stdlib.h>
 #include "functions.h"
 
-int main() {
-
-    int a, b, s;
-  
-  // ENTRADA DE DADOS
-  scanf("%d %d", &a, &b);
-
-  // PROCESSAMENTO - EXECUTANDO A FUNCAO
-  s = somar(a, b);
-
-  // SAIDA - IMPRIMINDO O RESULTADO
-  printf("SOMA = %d\n", s);
-
-  return(0);
+int main()
+{
+    //DECLARA AS VARIAVEIS.
+    int max=0,men,x;
+    //APRESENTA A FUNCAO DO ALGORITMO E SOLICITA A ENTRADA DE DADOS.
+    printf("Menor elemento de vetor!\n");
+    printf("Digite o numero de elementos.\n");
+    //ENTRADA DE DADOS.
+    scanf("%d",&max);
+    //DECLARA VARIAVEL VET E USA DO FOR PARA PREENCHER O VETOR.
+    int vet[max];
+    for(x=0;x<max;x++){
+        printf("Digite os elementos.\n");
+        scanf("%d",&vet[x]);
+    }
+    //EXECUTA A FUNCAO
+    men = menor(vet,max);
+    //IMPRIME A RESPOSTA
+    printf("MENOR = %d",men);
+    return 0;
 }
