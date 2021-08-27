51416235957ad92abc9c1ef6944f38012b2a9775
gabrielSabato <88863473+gabrielSabato@users.noreply.github.com>
2021-08-27 15:09:59 -0300

    Update functions.c

--- exe02/functions.c
+++ exe02/functions.c
@@ -1,7 +1,12 @@
 
 #include "functions.h"
 
-int somar(int n1, int n2){
-
-    return n1+n2;
+int menor(int v[],int m){
+    int menor = v[1];
+    for(int i=0;i<m;i++){
+        if(v[i]<menor){
+            menor = v[i];
+        }
+    }
+    return menor;
 }
